a9f6ce039c5c11bebd467dfe417303d94d9d9182
Ben Regan <dsrpsa1234@gmail.com>
2023-06-10 13:19:35 +0800

    Updated to recent forked repo, divided no_pi into 2 files

---
+++ smartpark/car_park_display.py
@@ -0,0 +1,46 @@
+import random
+import threading
+import time
+import tkinter as tk
+from typing import Iterable
+import paho.mqtt.client as paho
+from no_pi import WindowedDisplay
+
+class CarParkDisplay:
+    """Provides a simple display of the car park status. This is a skeleton only. The class is designed to be customizable without requiring and understanding of tkinter or threading."""
+    # determines what fields appear in the UI
+    fields = ['Available bays', 'Temperature', 'At']
+
+    def on_message_callback(self, client, userdata, message):
+        msg = message
+        msg_data = str(msg.payload.decode("UTF-8"))
+        print(msg_data)
+        return msg_data
+
+    def __init__(self):
+        self.window = WindowedDisplay(
+            'Joondalup', CarParkDisplay.fields)
+        updater = threading.Thread(target=self.check_updates)
+        updater.daemon = True
+        updater.start()
+        self.window.show()
+
+    def check_updates(self):
+        # TODO: This is where you should manage the MQTT subscription
+        available_bays = 192
+        while True:
+            # NOTE: Dictionary keys *must* be the same as the class fields
+            field_values = dict(zip(CarParkDisplay.fields, [
+                f'{available_bays}',
+                f'{random.randint(0, 45):02d}℃',
+                time.strftime("%H:%M:%S")]))
+            # Pretending to wait on updates from MQTT
+            time.sleep(random.randint(1, 10))
+            # When you get an update, refresh the display.
+            self.window.update(field_values)
+            client.loop_forever()
+
+
+if __name__ == '__main__':
+    # TODO: Run each of these classes in a separate terminal. You should see the CarParkDisplay update when you click the buttons in the CarDetector.
+    CarParkDisplay()
---
+++ smartpark/parking_lot.py
@@ -0,0 +1,15 @@
+class ParkingLot:
+
+    def __int__(self):
+        self.location = None #str
+        self.total_spaces = None # int
+        self.available_spaces = None # int
+        self.mqtt_client = None # MQTTClient
+
+        def enter(): -> None
+        pass
+        def exit(): -> None
+        pass
+        def publish_update(): -> None
+        pass
+
