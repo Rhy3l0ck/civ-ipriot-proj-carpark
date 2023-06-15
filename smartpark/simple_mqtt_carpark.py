7b04da21ded776b424be153f91222273964c287d
Ben Regan <dsrpsa1234@gmail.com>
2023-06-15 23:14:16 +0800

    parse_config updated name to config_parser, simple_mqtt_carpark works with toml format

--- smartpark/simple_mqtt_carpark.py
+++ smartpark/simple_mqtt_carpark.py
@@ -6,79 +6,70 @@ from paho.mqtt.client import MQTTMessage
 
 
 class CarPark(mqtt_device.MqttDevice):
-    """Creates a carpark object to store the state of cars in the lot"""
+    """Creates a carpark object to stor  the state of cars in the lot"""
 
     def __init__(self, config):
-        super().__init__(config)
+        super().__init__(config['broker'])
         carpark_name = config['broker']['location']
         self.total_spaces = config[carpark_name]['total-spaces']
         self.total_cars = config.get(f"{carpark_name}.total-cars", 0)
         self._temperature = None
-        print(f"Carpark ar {carpark_name} is ready")
+        print(f"Carpark at {carpark_name} is ready")
         self.client.on_message = self.on_message
         self.client.subscribe('sensor')
         self.client.loop_forever()
-        self._temperature = None
 
+    @property
+    def available_spaces(self):
+        available = self.total_spaces - self.total_cars
+        return max(available, 0)
 
-@property
-def available_spaces(self):
-    available = self.total_spaces - self.total_cars
-    return max(available, 0)
+    @property
+    def temperature(self):
+        self._temperature
 
+    @temperature.setter
+    def temperature(self, value):
+        self._temperature = value
 
-@property
-def temperature(self):
-    self._temperature
-
-
-@temperature.setter
-def temperature(self, value):
-    self._temperature = value
-
-
-def _publish_event(self):
-    readable_time = datetime.now().strftime('%H:%M')
-    print(
-        (
+    def _publish_event(self):
+        readable_time = datetime.now().strftime('%H:%M')
+        print(
+            (
                 f"TIME: {readable_time}, "
                 + f"SPACES: {self.available_spaces}, "
                 + "TEMPC: 42"
+            )
         )
-    )
-    message = (
+        message = (
             f"TIME: {readable_time}, "
             + f"SPACES: {self.available_spaces}, "
             + "TEMPC: 42"
-    )
-    self.client.publish('display', message)
+        )
+        self.client.publish('display', message)
 
+    def on_car_entry(self):
+        self.total_cars += 1
+        self._publish_event()
 
-def on_car_entry(self):
-    self.total_cars += 1
-    self._publish_event()
+    def on_car_exit(self):
+        self.total_cars -= 1
+        self._publish_event()
 
-
-def on_car_exit(self):
-    self.total_cars -= 1
-    self._publish_event()
-
-
-def on_message(self, client, userdata, msg: MQTTMessage):
-    payload = msg.payload.decode()
-    # TODO: Extract temperature from payload
-    # self.temperature = ... # Extracted  value
-    if 'exit' in payload:
-        self.on_car_exit()
-    else:
-        self.on_car_entry()
+    def on_message(self, client, userdata, msg: MQTTMessage):
+        payload = msg.payload.decode()
+        # TODO: Extract temperature from payload
+        # self.temperature = ... # Extracted  value
+        if 'exit' in payload:
+            self.on_car_exit()
+        else:
+            self.on_car_entry()
 
 
 if __name__ == '__main__':
-    from parse_config import parse_config
+    from config_parser import parse_config
 
     config = parse_config("config.toml")
-    # TODO: Read config from file
-    car_park = CarPark(config['broker']['location'])
+    car_park = CarPark(config)
     print("Carpark initialized")
     print("Carpark initialized")
